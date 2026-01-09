cab402b54b9e4a8c4eb8bf63aa5ed5fd648b3e3e
sharulatha068-ai <sharulatha068@gmail.com>
2026-01-09 23:05:37 +0530

    Day 2: Backend + Frontend connected

--- Backend/python.py
+++ Backend/python.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/test")
+def test():
+    return jsonify({"message": "Backend connected!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
